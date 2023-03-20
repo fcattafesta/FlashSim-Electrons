4006e8722dbc58a9a53395360495089b5ad598ce
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-20 15:03:39 +0100

    Z pt bins

--- test/z_boson.py
+++ test/z_boson.py
@@ -3,17 +3,17 @@ import ROOT
 ROOT.gROOT.ProcessLine('#include "z_func.h"')
 
 cuts = [
-    "z_pt < 50",
-    "z_pt >= 50 && z_pt < 100",
-    "z_pt >= 100 && z_pt < 150",
-    "z_pt >= 150",
+    "Z_pt < 50",
+    "Z_pt >= 50 && Z_pt < 100",
+    "Z_pt >= 100 && Z_pt < 150",
+    "Z_pt >= 150",
 ]
 
 labels = [
     "p^{Z}_{T} < 50",
     "50 \le p^{Z}_{T} < 100",
-    "100 \le p_{T} < 150",
-    " p_{T} \ge 150",
+    "100 \le p^{Z}_{T} < 150",
+    "p^{Z}_{T} \ge 150",
 ]
 
 filenames = [f"figures/z_boson_{i}bin.pdf" for i in range(1, len(cuts) + 1)]
