0f41dfa444543b9293c97188763832a00c2a86c3
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-20 14:19:16 +0100

    Z pt bins

--- test/z_boson.C
+++ test/z_boson.C
@@ -13,8 +13,8 @@ auto InvariantMass(ROOT::VecOps::RVec<double> &pt,
 }
 
 auto InvariantMass_float(ROOT::VecOps::RVec<float> &pt,
-                   ROOT::VecOps::RVec<float> &eta,
-                   ROOT::VecOps::RVec<float> &phi) {
+                         ROOT::VecOps::RVec<float> &eta,
+                         ROOT::VecOps::RVec<float> &phi) {
 
   auto m = 0.51099895000e-3; // GeV
 
@@ -22,6 +22,28 @@ auto InvariantMass_float(ROOT::VecOps::RVec<float> &pt,
   ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], m);
 
   return (p1 + p2).mass();
+}
+
+auto z_pt_float(ROOT::VecOps::RVec<float> &pt, ROOT::VecOps::RVec<float> &eta,
+                ROOT::VecOps::RVec<float> &phi) {
+
+  auto m = 0.51099895000e-3; // GeV
+
+  ROOT::Math::PtEtaPhiMVector p1(pt[0], eta[0], phi[0], m);
+  ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], m);
+
+  return (p1 + p2).pt();
+}
+
+auto z_pt(ROOT::VecOps::RVec<double> &pt, ROOT::VecOps::RVec<double> &eta,
+          ROOT::VecOps::RVec<double> &phi) {
+
+  auto m = 0.51099895000e-3; // GeV
+
+  ROOT::Math::PtEtaPhiMVector p1(pt[0], eta[0], phi[0], m);
+  ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], m);
+
+  return (p1 + p2).pt();
 }
 
 void z_boson() {
@@ -44,63 +66,80 @@ void z_boson() {
 
   auto d_flash = ROOT::RDataFrame("Events", f);
 
-  auto d_flash_z = d_flash.Filter("nElectron == 2")
-                 .Filter("All(abs(Electron_eta) < 2.5)")
-                 .Filter("All(Electron_pt > 20)")
-                 .Filter("Sum(Electron_charge) == 0")
-                 //.Filter("All(Electron_ip3d < 0.015)")
-                 .Define("Z_mass", InvariantMass,
-                         {"Electron_pt", "Electron_eta", "Electron_phi"});
+  auto d_flash_z =
+      d_flash.Filter("nElectron == 2")
+          .Filter("All(abs(Electron_eta) < 2.5)")
+          .Filter("All(Electron_pt > 20)")
+          .Filter("Sum(Electron_charge) == 0")
+          //.Filter("All(Electron_ip3d < 0.015)")
+          .Define("Z_pt", z_pt, {"Electron_pt", "Electron_eta", "Electron_phi"})
+          .Define("Z_mass", InvariantMass,
+                  {"Electron_pt", "Electron_eta", "Electron_phi"});
 
-  auto d_full_z = d_full.Filter("MnElectron == 2")
-                       .Filter("All(abs(MElectron_eta) < 2.5)")
-                       .Filter("All(MElectron_pt > 20)")
-                       .Filter("Sum(MElectron_charge) == 0")
-                       //.Filter("All(MElectron_ip3d < 0.015)")
-                       .Define("Z_mass", InvariantMass_float,
-                               {"MElectron_pt", "MElectron_eta", "MElectron_phi"});
+  auto d_full_z =
+      d_full.Filter("MnElectron == 2")
+          .Filter("All(abs(MElectron_eta) < 2.5)")
+          .Filter("All(MElectron_pt > 20)")
+          .Filter("Sum(MElectron_charge) == 0")
+          //.Filter("All(MElectron_ip3d < 0.015)")
+          .Define("Z_pt", z_pt_float,
+                  {"MElectron_pt", "MElectron_eta", "MElectron_phi"})
+          .Define("Z_mass", InvariantMass_float,
+                  {"MElectron_pt", "MElectron_eta", "MElectron_phi"});
 
-  auto h_flash = d_flash_z.Histo1D({"", "", 50, 60, 110}, "Z_mass");
-  auto h_full = d_full_z.Histo1D({"", "", 50, 60, 110}, "Z_mass");
+  std::string cuts = {"Z_pt < 50", "50 <= Z_pt < 100", "100 <= Z_pt < 150",
+                      "Z_pt >= 200"};
 
-  h_flash->Scale(1. / h_flash->Integral());
-  h_full->Scale(1. / h_full->Integral());
-  
-  gStyle->SetOptStat(0);
-  gStyle->SetTextFont(42);
-  auto c = new TCanvas("", "", 800, 700);
-  c->SetLeftMargin(0.15);
+  std::string label = {"p^Z_T < 50", "50 <= p^Z_T < 100", "100 <= p^Z_T < 150",
+                       "p^Z_T >= 200"};
 
-  h_full->SetTitle("");
-  h_full->GetXaxis()->SetTitle("m_{ee} [GeV]");
-  h_full->GetXaxis()->SetTitleSize(0.04);
-  h_full->GetYaxis()->SetTitle("Normalized Events / 1 GeV");
-  h_full->GetYaxis()->SetTitleSize(0.04);
-  h_full->SetLineColor(kBlack);
-  h_full->SetLineWidth(2);
-  h_full->SetLineStyle(2);
+  for (auto i = 0; i < cuts.size(); ++i) {
+    auto h_flash = d_flash_z.Filter(cuts[i].c_str())
+                       .Histo1D({"", "", 50, 60, 110}, "Z_mass");
+    auto h_full = d_full_z.Filter(cuts[i].c_str())
+                      .Histo1D({"", "", 50, 60, 110}, "Z_mass");
 
-  h_flash->SetLineColor(kOrange + 7);
-  h_flash->SetLineWidth(2);
+    h_flash->Scale(1. / h_flash->Integral());
+    h_full->Scale(1. / h_full->Integral());
 
-  h_full->DrawClone("hist");
-  h_flash->DrawClone("hist same");
+    gStyle->SetOptStat(0);
+    gStyle->SetTextFont(42);
+    auto c = new TCanvas("", "", 800, 700);
+    c->SetLeftMargin(0.15);
 
-  auto legend = new TLegend(0.72, 0.75, 0.89, 0.88);
-  legend->SetFillColor(0);
-  legend->SetBorderSize(0);
-  legend->SetTextSize(0.02);
-  legend->AddEntry(h_full.GetPtr(), "FullSim", "l");
-  legend->AddEntry(h_flash.GetPtr(), "FlashSim", "l");
-  legend->SetFillStyle(0);
-  legend->DrawClone("NDC NB");
+    h_full->SetTitle("");
+    h_full->GetXaxis()->SetTitle("m_{ee} [GeV]");
+    h_full->GetXaxis()->SetTitleSize(0.04);
+    h_full->GetYaxis()->SetTitle("Normalized Events / 1 GeV");
+    h_full->GetYaxis()->SetTitleSize(0.04);
+    h_full->SetLineColor(kBlack);
+    h_full->SetLineWidth(2);
+    h_full->SetLineStyle(2);
 
-  TLatex cms_label;
-  cms_label.SetTextSize(0.04);
-  cms_label.DrawLatexNDC(0.16, 0.92, "#bf{CMS} #it{Private Work}");
-  TLatex header;
-  header.SetTextSize(0.03);
+    h_flash->SetLineColor(kOrange + 7);
+    h_flash->SetLineWidth(2);
 
-  c->SaveAs("figures/z_boson.pdf");
+    h_full->DrawClone("hist");
+    h_flash->DrawClone("hist same");
 
+    auto legend = new TLegend(0.72, 0.75, 0.89, 0.88);
+    legend->SetFillColor(0);
+    legend->SetBorderSize(0);
+    legend->SetTextSize(0.02);
+    legend->AddEntry(h_full.GetPtr(), "FullSim", "l");
+    legend->AddEntry(h_flash.GetPtr(), "FlashSim", "l");
+    legend->SetFillStyle(0);
+    legend->DrawClone("NDC NB");
+
+    TLatex cms_label;
+    cms_label.SetTextSize(0.04);
+    cms_label.DrawLatexNDC(0.16, 0.92, "#bf{CMS} #it{Private Work}");
+    TLatex bin;
+    bin.SetTextSize(0.03);
+    bin.DrawLatexNDC(0.16, 0.86, cuts[i].c_str());
+
+    std::string filename = "figures/z_boson_" + std::to_string(i) + ".pdf";
+
+    c->SaveAs(filename.c_str());
+  }
 }
