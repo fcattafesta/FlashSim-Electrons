828b0ca12ebfde7ac8a48cbcfe24b47a7fecf03f
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-21 17:10:23 +0100

    classifier dataset

--- efficiencies/extract.py
+++ efficiencies/extract.py
@@ -1,5 +1,21 @@
 import os
+import h5py
+import pandas as pd
 import ROOT
+import uproot
+
+
+def dataset(tree, *args, **kwargs):
+
+    df = (
+        tree.arrays(library="pd", *args, **kwargs)
+        .reset_index(drop=True)
+        .astype("float32")
+        .dropna()
+    )
+
+    return df
+
 
 ROOT.gInterpreter.ProcessLine('#include "extraction.h"')
 
@@ -13,12 +29,26 @@ ttbar_training_files = [
 
 file_paths = [os.path.join(root, f) for f in ttbar_training_files]
 
-print(file_paths)
-
 outputs = [
-    os.path.join(os.path.dirname(__file__), "dataset", f"MGenElectrons_{i}")
+    os.path.join(os.path.dirname(__file__), "dataset", f"MGenElectrons_{i}.root")
     for i in range(len(file_paths))
 ]
 
-for file, output in zip(file_paths, outputs):
-    ROOT.extract(file, output)
+# for file, output in zip(file_paths, outputs):
+#     ROOT.extract(file, output)
+
+root_files = [f"{output}:GenElectrons" for output in outputs]
+
+tree = uproot.open(root_files[0], num_workers=20)
+df = dataset(tree)
+
+for file in root_files[1:]:
+    tree = uproot.open(file, num_workers=20)
+    df = pd.concat([df, dataset(tree)], axis=0)
+    df.reset_index(drop=True)
+
+print(df.columns)
+
+file = h5py.File(f"GenElectrons.hdf5", "w")
+file.create_dataset("GenElectrons", data=df.values, dtype="f4")
+file.close()
