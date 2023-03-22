1c9c4f99f8846cfa3ce5f1a430262dfcd06838be
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-22 14:05:16 +0100

    classifier

--- efficiencies/classifier/model.py
+++ efficiencies/classifier/model.py
@@ -54,17 +54,16 @@ def train(dataloader, model, loss_fn, optimizer, device):
 
 
 def test(dataloader, model, loss_fn, device):
-    num_batches = len(dataloader)
     model.eval()
-    test_loss, correct = 0, 0
+    test_loss = 0
+    accuracy = 0
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
-            pred_tag = torch.round(torch.sigmoid(pred))
-            correct = (pred_tag == y).sum().float().item()
-    test_loss /= num_batches
+            accuracy += accuracy(pred, y).item()
+
     print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+        f"Test Loss: {test_loss/len(dataloader)} | Test Accuracy: {accuracy/len(dataloader)}"
     )
