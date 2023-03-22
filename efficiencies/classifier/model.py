b8049eebe5ff4fc3503ba2eedfceeeddb6c5ddd1
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-22 11:49:45 +0100

    classifier

--- efficiencies/classifier/model.py
+++ efficiencies/classifier/model.py
@@ -16,7 +16,7 @@ class Classifier(nn.Module):
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
-        x = self.fc2(x)
+        x = self.fc3(x)
         x = self.softmax(x)
         return x
 
