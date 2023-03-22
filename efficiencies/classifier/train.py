dbc8a7d94dd0612d1169018b3b2bfab5cbc7bd59
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-22 14:31:04 +0100

    classifier

--- efficiencies/classifier/train.py
+++ efficiencies/classifier/train.py
@@ -12,7 +12,7 @@ def training_loop():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Device: {device}")
 
-    model = BinaryClassifier(38, 128).to(device)
+    model = BinaryClassifier(38, 512).to(device)
     print(f"Parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
     
     loss_fn = nn.BCEWithLogitsLoss()
