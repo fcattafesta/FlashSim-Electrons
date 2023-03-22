3f31daf4c65e0e5e7a2a3773243dd94d0097d324
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-22 09:57:22 +0100

    classifier

---
+++ efficiencies/classifier/model.py
@@ -0,0 +1,27 @@
+import os
+import h5py
+import torch
+
+class isReco_Dataset(torch.utils.data.Dataset):
+    def __init__(self, filepath, start, stop):
+        h5py_file = h5py.File(filepath, "r")
+        self.X = torch.tensor(
+            h5py_file["GenElectrons"][start : (start + stop), 0:38], dtype=torch.float32
+        )
+        self.y = torch.tensor(
+            h5py_file["GenElectrons"][start : (start + stop), -1], dtype=torch.long
+        )
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+    
+
+if __name__ == "__main__":
+
+    path = os.path.join(os.path.dirname(__file__), "..", "GenElectrons.hdf5")
+    data = isReco_Dataset(path, 0, 1000)
+
+    print(data)
