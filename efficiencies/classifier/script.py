d412703b5792d7f8517bda95854ae1ec028a5ae2
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-23 11:55:46 +0100

    state_dict

--- efficiencies/classifier/script.py
+++ efficiencies/classifier/script.py
@@ -11,7 +11,7 @@ datapath = os.path.join(os.path.dirname(__file__), "..", "dataset", "GenElectron
 dataset = isReco_Dataset(datapath, 3400000, 4400000)
 
 model = BinaryClassifier(38, 512)
-model.load_state_dict("model.pt")
+model.load_state_dict(torch.load("model.pt"))
 model.eval()
 
 example, _ = dataset[0]
