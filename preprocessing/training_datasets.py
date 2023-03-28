29480dad4c4ff21e8b41004b0fee558a6b50a729
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-28 16:45:00 +0200

    preprocessing

--- preprocessing/training_datasets.py
+++ preprocessing/training_datasets.py
@@ -4,30 +4,30 @@ from preprocessing import make_dataset
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "utils"))
 
-from columns import gen_ele, gen_pho, gen_jet, pu
+from columns import gen_ele, gen_pho, gen_jet
 
 dataset_path = os.path.join(os.path.dirname(__file__), "..", "extraction", "dataset")
 
-# # GenElectron training dataset
+# GenElectron training dataset
 
-# filenames = [f"MElectrons_{i}_ele.root:MElectrons" for i in range(9)]
+filenames = [f"MElectrons_{i}_ele.root:MElectrons" for i in range(9)]
 
-# filepaths = [os.path.join(dataset_path, f) for f in filenames]
+filepaths = [os.path.join(dataset_path, f) for f in filenames]
 
-# scale_name = "scale_factors_ele.json"
+scale_name = "scale_factors_ele.json"
 
-# make_dataset(filepaths, "MElectrons_ele", scale_name, gen_ele)
+make_dataset(filepaths, "MElectrons_ele", scale_name, gen_ele)
 
 
-# # GenPhoton training dataset
+# GenPhoton training dataset
 
-# filenames = [f"MElectrons_{i}_pho.root:MElectrons" for i in range(9)]
+filenames = [f"MElectrons_{i}_pho.root:MElectrons" for i in range(9)]
 
-# filepaths = [os.path.join(dataset_path, f) for f in filenames]
+filepaths = [os.path.join(dataset_path, f) for f in filenames]
 
-# scale_name = "scale_factors_pho.json"
+scale_name = "scale_factors_pho.json"
 
-# make_dataset(filepaths, "MElectrons_pho", scale_name, gen_pho)
+make_dataset(filepaths, "MElectrons_pho", scale_name, gen_pho)
 
 
 # GenJet training dataset
@@ -37,9 +37,5 @@ filenames = [f"MElectrons_{i}_jet.root:MElectrons" for i in range(9)]
 filepaths = [os.path.join(dataset_path, f) for f in filenames]
 
 scale_name = "scale_factors_jet.json"
-
-for s in pu:
-    gen_jet.remove(s)
-
 
 make_dataset(filepaths, "MElectrons_jet", scale_name, gen_jet)
