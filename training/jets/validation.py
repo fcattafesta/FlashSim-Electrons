b472122342ff564a510067798eb8c648889dd0c1
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-30 09:54:56 +0200

    bug in jets validation

--- training/jets/validation.py
+++ training/jets/validation.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "postproc
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "utils"))
 
 from postprocessing import postprocessing
-from post_actions_jet import target_dictionary, context_dictionary
+from post_actions_jet import target_dictionary
 from corner_plots import make_corner
 
 from columns import gen_jet, reco_columns
