d6618d2f60048926d0a00efd2fd6f652c82923fa
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-28 11:27:54 +0200

    Match

--- test/test.py
+++ test/test.py
@@ -19,30 +19,49 @@ if __name__ == "__main__":
 
     rdf = ROOT.RDataFrame(events)
 
+    rdf = (
+        rdf.Define("GenPart_ElectronIdx_empty", "Electron_genObjMatchMaker(GenPart_pt)")
+        .Define(
+            "GenPart_genElectron_ElectronIdx",
+            "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, FullSim.Electron_pt, FullSim.Electron_eta, FullSim.Electron_phi, FullSim.Electron_charge, GenPart_ElectronIdx_empty, 0)",
+        )
+        .Define("GenPart_isLastCopy", "BitwiseDecoder(GenPart_statusFlags, 13)")
+        .Define("GenElectronMask", "abs(GenPart_pdgId) == 11 && GenPart_isLastCopy")
+        .Define("GenElectron_pt", "GenPart_pt[GenElectronMask]")
+        .Define(
+            "GenElectron_FullMatched",
+            "GenPart_genElectron_ElectronIdx[GenElectronMask] >= 0",
+        )
+        .Define(
+            "MElectron_pt",
+            "Take(FullSim.Electron_pt, GenPart_genElectron_ElectronIdx[GenElectronMask])",
+        )
+    )
+
     c_pt = comparison(rdf, "Electron_pt", [0, 100], 100)
     c_pt.SaveAs(os.path.join(save_path, "Electron_pt.pdf"))
 
     # Z boson
 
-    cuts = [
-        "Z_pt < 50",
-        "Z_pt >= 50 && Z_pt < 100",
-        "Z_pt >= 100 && Z_pt < 150",
-        "Z_pt >= 150",
-        "1",
-    ]
+    # cuts = [
+    #     "Z_pt < 50",
+    #     "Z_pt >= 50 && Z_pt < 100",
+    #     "Z_pt >= 100 && Z_pt < 150",
+    #     "Z_pt >= 150",
+    #     "1",
+    # ]
 
-    labels = [
-        "p^{Z}_{T} < 50",
-        "50 #leq p^{Z}_{T} < 100",
-        "100 #leq p^{Z}_{T} < 150",
-        "p^{Z}_{T} #geq 150",
-        "",
-    ]
+    # labels = [
+    #     "p^{Z}_{T} < 50",
+    #     "50 #leq p^{Z}_{T} < 100",
+    #     "100 #leq p^{Z}_{T} < 150",
+    #     "p^{Z}_{T} #geq 150",
+    #     "",
+    # ]
 
-    df_full = ROOT.RDataFrame(full)
+    # df_full = ROOT.RDataFrame(full)
 
-    for i, (cut, label) in enumerate(zip(cuts, labels)):
-        filename = f"z_{i}_bin.pdf"
-        c_z = analysis(df_full, rdf, cut, label)
-        c_z.SaveAs(os.path.join(save_path, filename))
+    # for i, (cut, label) in enumerate(zip(cuts, labels)):
+    #     filename = f"z_{i}_bin.pdf"
+    #     c_z = analysis(df_full, rdf, cut, label)
+    #     c_z.SaveAs(os.path.join(save_path, filename))
