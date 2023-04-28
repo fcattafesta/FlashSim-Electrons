f9def11494b1ae066894ffb1d72295b67ad2c87f
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-28 11:53:51 +0200

    Match

--- test/test.py
+++ test/test.py
@@ -22,22 +22,12 @@ if __name__ == "__main__":
     rdf = ROOT.RDataFrame(events)
 
     rdf = (
-        rdf.Define("GenPart_ElectronIdx_empty", "Electron_genObjMatchMaker(GenPart_pt)")
-        .Define(
-            "GenPart_genElectron_ElectronIdx",
-            "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, FullSim.Electron_pt, FullSim.Electron_eta, FullSim.Electron_phi, FullSim.Electron_charge, GenPart_ElectronIdx_empty, 0)",
+        rdf.Define(
+            "GenPart_ElectronIdx",
+            "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, FullSim.Electron_pt, FullSim.Electron_eta, FullSim.Electron_phi, FullSim.Electron_charge)",
         )
-        .Define("GenPart_isLastCopy", "BitwiseDecoder(GenPart_statusFlags, 13)")
-        .Define("GenElectronMask", "abs(GenPart_pdgId) == 11 && GenPart_isLastCopy")
-        .Define("GenElectron_pt", "GenPart_pt[GenElectronMask]")
-        .Define(
-            "GenElectron_FullMatched_Idx",
-            "GenPart_genElectron_ElectronIdx[GenElectronMask] >= 0",
-        )
-        .Define(
-            "MElectron_pt",
-            "Take(FullSim.Electron_pt, GenElectron_FullMatched_Idx)",
-        )
+        .Define("MatchedIdx", "GenPart_ElectronIdx[GenPart_ElectronIdx >= 0]")
+        .Define("MElectron_pt", "Take(FullSim.Electron_pt, MatchedIdx)")
     )
 
     c_pt = comparison(rdf, "Electron_pt", [0, 100], 100)
