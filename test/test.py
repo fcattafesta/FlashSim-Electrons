e5c5dcfc2cb6e8da164442550ffc541559ea78a2
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-02 13:57:36 +0200

    pt

--- test/test.py
+++ test/test.py
@@ -13,75 +13,75 @@ if __name__ == "__main__":
 
     ROOT.EnableImplicitMT()
 
-    # file = ROOT.TFile.Open(path)
+    file = ROOT.TFile.Open(path)
 
-    # events = file.Events
-    # full = file.FullSim
+    events = file.Events
+    full = file.FullSim
 
-    # events.AddFriend(full, "FullSim")
+    events.AddFriend(full, "FullSim")
 
-    # rdf = ROOT.RDataFrame(events)
+    rdf = ROOT.RDataFrame(events)
 
-    # rdf = (
-    #     rdf.Define(
-    #         "MGenPart_ElectronIdx",
-    #         "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, FullSim.Electron_pt, FullSim.Electron_eta, FullSim.Electron_phi, FullSim.Electron_charge)",
-    #     )
-    #     .Define("MatchedIdx", "MGenPart_ElectronIdx[MGenPart_ElectronIdx >= 0]")
-    #     .Define("MGenElectron_pt", "GenPart_pt[MGenPart_ElectronIdx >= 0]")
-    #     .Define("MElectron_pt", "Take(FullSim.Electron_pt, MatchedIdx)")
-    #     .Define("MRatio", "MElectron_pt / MGenElectron_pt")
-    #     .Define("PGenPart_ElectronIdx", "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, Electron_pt, Electron_eta, Electron_phi, Electron_charge)")
-    #     .Define("PMatchedIdx", "PGenPart_ElectronIdx[PGenPart_ElectronIdx >= 0]")
-    #     .Define("PGenElectron_pt", "GenPart_pt[PGenPart_ElectronIdx >= 0]")
-    #     .Define("PElectron_pt", "Take(Electron_pt, PMatchedIdx)")
-    #     .Define("PRatio", "PElectron_pt / PGenElectron_pt * 0.97")
-    # )
+    rdf = (
+        rdf.Define(
+            "MGenPart_ElectronIdx",
+            "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, FullSim.Electron_pt, FullSim.Electron_eta, FullSim.Electron_phi, FullSim.Electron_charge)",
+        )
+        .Define("MatchedIdx", "MGenPart_ElectronIdx[MGenPart_ElectronIdx >= 0]")
+        .Define("MGenElectron_pt", "GenPart_pt[MGenPart_ElectronIdx >= 0]")
+        .Define("MElectron_pt", "Take(FullSim.Electron_pt, MatchedIdx)")
+        .Define("MRatio", "MElectron_pt / MGenElectron_pt")
+        .Define("PGenPart_ElectronIdx", "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, Electron_pt, Electron_eta, Electron_phi, Electron_charge)")
+        .Define("PMatchedIdx", "PGenPart_ElectronIdx[PGenPart_ElectronIdx >= 0]")
+        .Define("PGenElectron_pt", "GenPart_pt[PGenPart_ElectronIdx >= 0]")
+        .Define("PElectron_pt", "Take(Electron_pt, PMatchedIdx)")
+        .Define("PRatio", "PElectron_pt / PGenElectron_pt * 0.97")
+    )
 
-    # h_full = rdf.Histo1D(("h_full", "h_full", 100, 0.5, 1.5), "MRatio")
-    # h_full.Scale(1 / h_full.Integral())
-    # h_flash = rdf.Histo1D(("h_flash", "h_flash", 100, 0.5, 1.5), "PRatio")
-    # h_flash.Scale(1 / h_flash.Integral())
+    h_full = rdf.Histo1D(("h_full", "h_full", 100, 0.5, 1.5), "MRatio")
+    h_full.Scale(1 / h_full.Integral())
+    h_flash = rdf.Histo1D(("h_flash", "h_flash", 100, 0.5, 1.5), "PRatio")
+    h_flash.Scale(1 / h_flash.Integral())
 
-    # ROOT.gStyle.SetOptStat(0)
-    # ROOT.gStyle.SetOptFit(0)
-    # ROOT.gStyle.SetTextFont(42)
-    # c = ROOT.TCanvas("c", "c", 800, 700)
-    # c.SetLeftMargin(0.15)
+    ROOT.gStyle.SetOptStat(0)
+    ROOT.gStyle.SetOptFit(0)
+    ROOT.gStyle.SetTextFont(42)
+    c = ROOT.TCanvas("c", "c", 800, 700)
+    c.SetLeftMargin(0.15)
 
-    # h_full.SetTitle("")
-    # h_full.GetXaxis().SetTitle("p_{T} / p_{T}^{G}")
-    # h_full.GetXaxis().SetTitleSize(0.04)
-    # h_full.GetYaxis().SetTitle("Normalized Events")  # pt comparison
-    # h_full.GetYaxis().SetTitleSize(0.04)
-    # h_full.SetLineColor(ROOT.kBlack)
-    # h_full.SetLineWidth(2)
-    # h_full.SetLineStyle(2)
+    h_full.SetTitle("")
+    h_full.GetXaxis().SetTitle("p_{T} / p_{T}^{G}")
+    h_full.GetXaxis().SetTitleSize(0.04)
+    h_full.GetYaxis().SetTitle("Normalized Events")  # pt comparison
+    h_full.GetYaxis().SetTitleSize(0.04)
+    h_full.SetLineColor(ROOT.kBlack)
+    h_full.SetLineWidth(2)
+    h_full.SetLineStyle(2)
 
-    # h_flash.SetLineColor(ROOT.kOrange + 7)
-    # h_flash.SetLineWidth(2)
+    h_flash.SetLineColor(ROOT.kOrange + 7)
+    h_flash.SetLineWidth(2)
 
-    # h_full.DrawClone("hist")
-    # h_flash.DrawClone("hist same")
+    h_full.DrawClone("hist")
+    h_flash.DrawClone("hist same")
 
-    # legend = ROOT.TLegend(0.72, 0.75, 0.89, 0.88)
-    # legend.SetFillColor(0)
-    # legend.SetFillStyle(0)
-    # legend.SetBorderSize(0)
-    # legend.SetTextSize(0.02)
-    # legend.AddEntry("h_full", "FullSim", "l")
-    # legend.AddEntry("h_flash", "FlashSim", "l")
-    # legend.DrawClone("NDC NB")
+    legend = ROOT.TLegend(0.72, 0.75, 0.89, 0.88)
+    legend.SetFillColor(0)
+    legend.SetFillStyle(0)
+    legend.SetBorderSize(0)
+    legend.SetTextSize(0.02)
+    legend.AddEntry("h_full", "FullSim", "l")
+    legend.AddEntry("h_flash", "FlashSim", "l")
+    legend.DrawClone("NDC NB")
 
-    # cms_label = ROOT.TLatex()
-    # cms_label.SetTextSize(0.04)
-    # cms_label.DrawLatexNDC(0.16, 0.92, "#bf{CMS} #it{Private Work}")
-    # c.Update()
+    cms_label = ROOT.TLatex()
+    cms_label.SetTextSize(0.04)
+    cms_label.DrawLatexNDC(0.16, 0.92, "#bf{CMS} #it{Private Work}")
+    c.Update()
 
-    # c.SaveAs(os.path.join(save_path, "DY_ratio.pdf"))
+    c.SaveAs(os.path.join(save_path, "DY_ratio.pdf"))
 
-    # c_pt = comparison(rdf, "Electron_pt", [0, 100], 100)
-    # c_pt.SaveAs(os.path.join(save_path, "DY_Electron_pt.pdf"))
+    c_pt = comparison(rdf, "Electron_pt", [0, 100], 100)
+    c_pt.SaveAs(os.path.join(save_path, "DY_Electron_pt.pdf"))
 
     # c_1 = ratio(rdf, "PElectron_pt", "PGenElectron_pt", "FlashSim")
     # c_1.SaveAs(os.path.join(save_path, "TT_2D_pt_Flash.pdf"))
@@ -92,27 +92,27 @@ if __name__ == "__main__":
 
     # Z boson
 
-    rdf = ROOT.RDataFrame("Events", path)
+    # rdf = ROOT.RDataFrame("Events", path)
 
-    cuts = [
-        "Z_pt < 50",
-        "Z_pt >= 50 && Z_pt < 100",
-        "Z_pt >= 100 && Z_pt < 150",
-        "Z_pt >= 150",
-        "1",
-    ]
+    # cuts = [
+    #     "Z_pt < 50",
+    #     "Z_pt >= 50 && Z_pt < 100",
+    #     "Z_pt >= 100 && Z_pt < 150",
+    #     "Z_pt >= 150",
+    #     "1",
+    # ]
 
-    labels = [
-        "p^{Z}_{T} < 50",
-        "50 #leq p^{Z}_{T} < 100",
-        "100 #leq p^{Z}_{T} < 150",
-        "p^{Z}_{T} #geq 150",
-        "",
-    ]
+    # labels = [
+    #     "p^{Z}_{T} < 50",
+    #     "50 #leq p^{Z}_{T} < 100",
+    #     "100 #leq p^{Z}_{T} < 150",
+    #     "p^{Z}_{T} #geq 150",
+    #     "",
+    # ]
 
-    df_full = ROOT.RDataFrame("FullSim", path)
+    # df_full = ROOT.RDataFrame("FullSim", path)
 
-    for i, (cut, label) in enumerate(zip(cuts, labels)):
-        filename = f"z_{i}_bin.pdf"
-        c_z = analysis(df_full, rdf, cut, label)
-        c_z.SaveAs(os.path.join(save_path, filename))
+    # for i, (cut, label) in enumerate(zip(cuts, labels)):
+    #     filename = f"z_{i}_bin.pdf"
+    #     c_z = analysis(df_full, rdf, cut, label)
+    #     c_z.SaveAs(os.path.join(save_path, filename))
